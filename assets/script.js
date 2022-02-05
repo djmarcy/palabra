73fc2ba32125dd947985d8858dfac54c9592889c
Jacob Lewis <jacobllewis@outlook.com>
2022-02-05 11:35:27 -0700

    updated translate api x3

--- assets/script.js
+++ assets/script.js
@@ -51,7 +51,10 @@ function testing() {
     }
   })
   .then(response => {
-    console.log(response);
+    response.json();
+  })
+  .then(data => {
+    console.log(data);
   })
   .catch(err => {
     console.error(err);
